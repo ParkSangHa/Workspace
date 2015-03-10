a39e03d88302cf1708b1f5e011275b6cfa40b833
ParkSangHa <havenpark@gmail.com>
2015-03-10 12:09:16 +0900

    Android Training upto GridView

--- AndroidWorkspace/HelloGridView/app/src/main/java/com/example/a/hellogridview/HelloGridView.java
+++ AndroidWorkspace/HelloGridView/app/src/main/java/com/example/a/hellogridview/HelloGridView.java
@@ -1,31 +1,36 @@
-package com.example.parksangha.myfirstapp;
+package com.example.a.hellogridview;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class HelloGridView extends ActionBarActivity {
 
-    public final static String EXTRA_MESSAGE = "com.parksangha.myfirstapp.MESSAGE";
-
-
-    // 콜백 메써드드
-   @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_hello_grid_view);
+
+        GridView gridView = (GridView)findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id){
+                Toast.makeText(HelloGridView.this, ""+position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_hello_grid_view, menu);
         return true;
     }
 
@@ -40,15 +45,7 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
-    }
-
-
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText)findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
